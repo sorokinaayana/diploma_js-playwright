fbb7a3c8c5b3d28b9a7f4409dd36f8a939d3e651
Yana <sorokinaa.yana@gmail.com>
2025-11-09 17:29:10 +0300

    исправлены зависимости

--- playwright.config.js
+++ playwright.config.js
@@ -6,12 +6,13 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
+  timeout: 30000,
   reporter: [
     ['html'],
     ['allure-playwright']
   ],
   use: {
-    baseURL: 'https://realworld.qa.guru', 
+    trace: 'on-first-retry',
     screenshot: 'only-on-failure',
   },
   projects: [
